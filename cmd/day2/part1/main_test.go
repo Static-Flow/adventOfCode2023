bdb05bf3161e2f1f24f7479e21bdb70cbf661fe5
Static-Flow <tanner989@gmail.com>
2023-12-02 21:44:58 -0600

    readmes and an even faster part1

--- cmd/day2/part1/main_test.go
+++ cmd/day2/part1/main_test.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"adventOfCode2023/internal"
+	"strings"
 	"testing"
 )
 
+var res bool
+
 func BenchmarkProcessGame(b *testing.B) {
 	var lines []string
+	var innerRes bool
 	if iterator, err := internal.NewLineIterator("../benchmarkData"); err == nil {
 		for iterator.Next() {
 			line := iterator.Line()
@@ -16,12 +20,11 @@ func BenchmarkProcessGame(b *testing.B) {
 		b.ResetTimer()
 		for _, line := range lines {
 			b.Run(line, func(b *testing.B) {
-				var sum int
+				pieces := strings.SplitN(line, ":", 2)
 				for i := 0; i < b.N; i++ {
-					if processGame(line) {
-						sum += gameNumber
-					}
+					innerRes = processGame(pieces[1])
 				}
+				res = innerRes
 			})
 		}
 	}
